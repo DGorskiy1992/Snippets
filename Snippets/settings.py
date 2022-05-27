3067423a1167f7cd45e0fc04ed8befef82802bfc
DGorskiy <dik.khailender@yandex.ru>
2022-05-27 19:57:09 +0300

    Settings for heroku

--- Snippets/settings.py
+++ Snippets/settings.py
@@ -11,10 +11,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
+}
+DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -125,3 +138,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR/"static"
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
